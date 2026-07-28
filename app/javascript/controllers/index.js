2a1105421f3a57d20314cb8c1855261194286c8c
Sedin-Jaya-Suriya <jaya.suriya@sedintechnologies.com>
2026-07-28 16:54:28 +0530

    Fix Stimulus controller import

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,4 @@
-import { application } from "./application"
-
+import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-eagerLoadControllersFrom(
-    "controllers",
-    application
-)
+eagerLoadControllersFrom("controllers", application)
