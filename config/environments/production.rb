085534c9185b423c30fe3c635783113c467f8a4a
Sedin-Jaya-Suriya <jaya.suriya@sedintechnologies.com>
2026-07-29 11:16:02 +0530

    Configure Resend email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,21 @@ Rails.application.configure do
     protocol: "https"
   }
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    domain: ENV["APP_HOST"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
   # Enable locale fallbacks.
   config.i18n.fallbacks = true
 
