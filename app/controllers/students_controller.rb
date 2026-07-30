f31531f68298af64722d441b8a22c97f349bfc6d
Sedin-Jaya-Suriya <jaya.suriya@sedintechnologies.com>
2026-07-30 11:12:12 +0530

    Remove invalid verify_authenticity_token callback

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,4 @@
 class StudentsController < ApplicationController
-  # skip_before_action :verify_authenticity_token, only: [ :create, :update ]
   before_action :set_student, only: [
     :show,
     :edit,
